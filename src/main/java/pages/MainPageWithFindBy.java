d37f24ddd22bbb096afa402ce85e7954815a2537
Anna Chernyshova <anna_chernyshova@epam.com>
2020-12-08 15:16:34 +0300

    added verifyWhyChooseUsList to verify healing of elements list

--- src/main/java/pages/MainPageWithFindBy.java
+++ src/main/java/pages/MainPageWithFindBy.java
@@ -4,16 +4,26 @@ package pages;
 import com.epam.healenium.SelfHealingDriver;
 import com.epam.healenium.annotation.DisableHealing;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MainPageWithFindBy extends BasePage {
 
-    @FindBy(xpath = "//input[@placeholder='E-mail']" )
+    @FindBy(xpath = "//input[@placeholder='E-mail']")
     private WebElement email;
+
+    @FindBy(xpath = "//div[contains(text(), 'Quality')]")
+    private List<WebElement> listElements;
+
 
     public MainPageWithFindBy(SelfHealingDriver driver) {
         super(driver);
@@ -41,8 +51,22 @@ public class MainPageWithFindBy extends BasePage {
     }
 
     public MainPageWithFindBy clickLearnMore() {
-        driver.findElement(By.xpath("//button[text()='Learn More']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        WebElement button = driver.findElement(By.xpath("//button[text()='Learn More']"));
+        wait.until(ExpectedConditions.visibilityOf(button));
+        button.click();
         return this;
+    }
+
+    public List<String> getTextFromWhyChooseUsList() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollBy(0,document.body.scrollHeight)");
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        List<WebElement> elements = driver.findElements(By.xpath("//div[contains(text(), 'Quality')]"));
+//        wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+        return elements.stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 
     public String getEmailErrorNotificationText() {
--- src/test/java/selenium/NotificationsTest.java
+++ src/test/java/selenium/NotificationsTest.java
@@ -1,26 +1,41 @@
 package selenium;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import pages.MainPageWithFindBy;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NotificationsTest extends BaseTest {
     private static final Logger LOGGER = LogManager.getLogger(NotificationsTest.class);
 
 
+//    @Test
+//    public void submitApplication() {
+//        LOGGER.info("submitApplication test start");
+//        MainPageWithFindBy mainPage = new MainPageWithFindBy(driver);
+//        mainPage.open()
+//            .inputEmailToSubmit("mail@mail")
+//            .inputNameToSubmit("Name")
+//            .inputPhoneToSubmit("89992346789").clickLearnMore();
+//        String notificationText = mainPage.getEmailErrorNotificationText();
+//        assertThat(notificationText, equalToIgnoringCase("Please put a correct e-mail"));
+//    }
+
     @Test
-    public void submitApplication() {
-        LOGGER.info("submitApplication test start");
+    public void verifyWhyChooseUsList() {
+        LOGGER.info("verifyWhyChooseUsList test start");
         MainPageWithFindBy mainPage = new MainPageWithFindBy(driver);
-        mainPage.open()
-            .inputEmailToSubmit("mail@mail")
-            .inputNameToSubmit("Name")
-            .inputPhoneToSubmit("89992346789").clickLearnMore();
-        String notificationText = mainPage.getEmailErrorNotificationText();
-        assertThat(notificationText, equalToIgnoringCase("Please put a correct e-mail"));
+        List<String> elementsText = mainPage.open()
+                .getTextFromWhyChooseUsList();
+        assertThat(elementsText.size(), is(3));
     }
+
+
 }
